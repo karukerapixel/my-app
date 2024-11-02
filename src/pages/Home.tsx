ba417ffed9adfba69b087e4916e1ab3a3a23b174
karukerapixel <karukerapixel@gmail.com>
2024-11-03 00:24:03 +0100

    importing hero, services and portfolio components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Portfolio from '../components/Portfolio';
 
 const Home: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Portfolio />
+    </>
+  );
 };
 
 export default Home;
